49aeea0bc3928ccf9a37200c16d497801a7f7a79
thabo_IOS <47thabo@gmail.com>
2024-07-01 11:08:25 +0200

    STYLE: Make the table columns width even

--- resources/views/sites/view.blade.php
+++ resources/views/sites/view.blade.php
@@ -6,40 +6,40 @@
     </x-slot>
 
     <div class="flex flex-wrap gap-y-4 px-3">
-        <table class="table w-full mt-1 rounded-lg shadow-md overflow-hidden">
-            <thead class="table-active bg-gray-400">
+        <table class="table-auto w-full mt-1 rounded-lg shadow-md overflow-hidden">
+            <thead class="bg-gray-400">
                 <tr class="text-left">
-                    <th class="px-2">Room#</th>
-                    <th class="px-2">Tenant</th>
-                    <th class="px-2">Cost</th>
-                    <th class="px-2">Action</th>
+                    <th class="px-4 py-2 w-1/4">Room#</th>
+                    <th class="px-4 py-2 w-1/4">Tenant</th>
+                    <th class="px-4 py-2 w-1/4">Cost</th>
+                    <th class="px-4 py-2 w-1/4">Action</th>
                 </tr>
             </thead>
             <tbody class="bg-white">
                 @foreach ($site->rooms as $room)
                     <tr class="border-t border-gray-300">
-                        <td class="px-2 py-2">{{ $room->name }} ( {{ $room->description }})</td>
-                        <td class="px-2">N/A</td>
-                        <td class="px-2">R2 500</td>
-                        <td class="px-2 flex h-full items-center"> <button
-                                class="bg-primary-600 flex items-centre text-black mr-1 px-2 py-1 rounded-md text-sm">
-                                <ion-icon name="add" class="size-4 text-black"></ion-icon>
-
-                                Assign tenant</button>
-                            <button class="bg-primary-600 text-black px-2 mr-1 py-1 flex items-center rounded-md text-sm">
-                                <ion-icon name="pencil" class="size-4 text-black"></ion-icon>
-
-                                Edit</button>
-                            <button class="bg-danger text-black px-2 py-1 mr-1 rounded-md text-sm flex items-center">
-                                <ion-icon name="trash" class="size-4 text-black"></ion-icon>
-
-                                Delete</button>
-
-
+                        <td class="px-4 py-2">{{ $room->name }} ( {{ $room->description }})</td>
+                        <td class="px-4 py-2">N/A</td>
+                        <td class="px-4 py-2">R{{ $room->cost }}</td>
+                        <td class="px-4 py-2">
+                            <div class="flex">
+                                <button
+                                    class="bg-primary-600 flex items-center text-black px-2 py-1 rounded-md text-sm mr-1">
+                                    <ion-icon name="add" class="text-black text-sm"></ion-icon> Assign tenant
+                                </button>
+                                <button
+                                    class="bg-primary-600 flex items-center text-black px-2 py-1 rounded-md text-sm mr-1">
+                                    <ion-icon name="pencil" class="text-black text-sm"></ion-icon> Edit
+                                </button>
+                                <button class="bg-danger flex items-center text-black px-2 py-1 rounded-md text-sm">
+                                    <ion-icon name="trash" class="text-black text-sm"></ion-icon> Delete
+                                </button>
+                            </div>
                         </td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
+
     </div>
 </x-app-layout>
