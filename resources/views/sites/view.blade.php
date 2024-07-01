9b199fc14d0d44a645e15f0d0bf496aba5110e0b
Slow <7slow7poison7@gmail.com>
2024-07-01 06:55:43 +0200

    STYLE: Further style the rooms table
    - I added 2 more columns and also making a border for each row

--- resources/views/sites/view.blade.php
+++ resources/views/sites/view.blade.php
@@ -6,18 +6,37 @@
     </x-slot>
 
     <div class="flex flex-wrap gap-y-4 px-3">
-        <table class="table mt-4">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Description</th>
+        <table class="table w-full mt-1 rounded-lg shadow-md overflow-hidden">
+            <thead class="table-active bg-gray-400">
+                <tr class="text-left">
+                    <th class="px-2">Room#</th>
+                    <th class="px-2">Tenant</th>
+                    <th class="px-2">Cost</th>
+                    <th class="px-2">Action</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody class="bg-white">
                 @foreach ($site->rooms as $room)
-                    <tr>
-                        <td>{{ $room->name }}</td>
-                        <td>{{ $room->description }}</td>
+                    <tr class="border-t border-gray-300">
+                        <td class="px-2 py-2">{{ $room->name }} ( {{ $room->description }})</td>
+                        <td class="px-2">N/A</td>
+                        <td class="px-2">R2 500</td>
+                        <td class="px-2 flex h-full items-center"> <button
+                                class="bg-primary-600 flex items-centre text-black mr-1 px-2 py-1 rounded-md text-sm">
+                                <ion-icon name="add" class="size-4 text-black"></ion-icon>
+
+                                Assign tenant</button>
+                            <button class="bg-primary-600 text-black px-2 mr-1 py-1 flex items-center rounded-md text-sm">
+                                <ion-icon name="pencil" class="size-4 text-black"></ion-icon>
+
+                                Edit</button>
+                            <button class="bg-danger text-black px-2 py-1 mr-1 rounded-md text-sm flex items-center">
+                                <ion-icon name="trash" class="size-4 text-black"></ion-icon>
+
+                                Delete</button>
+
+
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
