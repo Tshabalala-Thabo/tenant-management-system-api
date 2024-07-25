a68dbd0f0ab5aa6f05eb8e09ef556dd0dfeb4765
Thabo Tshabalala <7slow7poison7@gmail.com>
2024-07-25 17:13:16 +0200

    FIX: Replace the lease column with actions column

--- resources/views/tenants.blade.php
+++ resources/views/tenants.blade.php
@@ -14,32 +14,33 @@
                     <tr class="text-left">
                         <th class="px-4 py-2">Names</th>
                         <th class="px-4 py-2">Email</th>
+                        <th class="px-4 py-2">Phone</th>
                         <th class="px-4 py-2">Sites and Rooms</th>
-                        <th class="px-4 py-2">Lease</th>
+                        <th class="px-4 py-2">Actions</th>
                     </tr>
                 </thead>
                 <tbody class="bg-white">
                     @foreach($groupedTenants as $tenantId => $tenantRooms)
-                            <tr class="border-t border-gray-300 cursor-pointer"
-                                onclick="window.location='{{ route('tenants.show', $tenantRooms->first()['tenant']->id) }}'">
-                                <td class="px-4 py-2">{{ ucfirst($tenantRooms->first()['tenant']->name) }}
-                                    {{ ucfirst($tenantRooms->first()['tenant']->last_name) }}</td>
-                                <td class="px-4 py-2">{{ $tenantRooms->first()['tenant']->email }}</td>
-                                <td class="px-4 py-2 flex flex-wrap">
-                                    @foreach($tenantRooms->groupBy('site.id') as $siteId => $siteRooms)
-                                                    <strong class="mr-1">{{ $siteRooms->first()['site']->name }}:</strong>
-                                                    @php
-                                                        $roomNames = $siteRooms->pluck('room.name')->implode(', ');
-                                                    @endphp
-                                                    {{ $roomNames }} <span class="mr-3"></span>
-                                    @endforeach
-                                </td>
-                                <td class="px-4 py-2">N/A</td>
-                            </tr>
+                        <tr class="border-t border-gray-300 cursor-pointer" onclick="window.location='{{ route('tenants.show', $tenantRooms->first()['tenant']->id) }}'">
+                            <td class="px-4 py-2">{{ ucfirst($tenantRooms->first()['tenant']->name) }} {{ ucfirst($tenantRooms->first()['tenant']->last_name) }}</td>
+                            <td class="px-4 py-2">{{ $tenantRooms->first()['tenant']->email }}</td>
+                            <td class="px-4 py-2">{{ $tenantRooms->first()['tenant']->phone }}</td>
+                            <td class="px-4 py-2">
+                                @foreach($tenantRooms->groupBy('site.id') as $siteId => $siteRooms)
+                                    <strong class="mr-1/2">{{ $siteRooms->first()['site']->name }}:</strong>
+                                    @php
+                                        $roomNames = $siteRooms->pluck('room.name')->implode(', ');
+                                    @endphp
+                                    {{ $roomNames }} <span class="mr-3"></span>
+                                @endforeach
+                            </td>
+                            <td class="px-4 py-2">
+                                <ion-icon name="eye" class="size-5 text-gray-500"></ion-icon>
+                            </td>
+                        </tr>
                     @endforeach
                 </tbody>
             </table>
-
         @endif
     </div>
 </x-app-layout>
