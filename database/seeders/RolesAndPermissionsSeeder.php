37b85dd3409eb4e64edcc0dfabcf3982dd8ce823
Thabo Tshabalala <47thabo@gmail.com>
2024-08-02 12:04:09 +0200

    FEAT: assign view sites permission to service providers

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -31,6 +31,7 @@ class RolesAndPermissionsSeeder extends Seeder
         // Assign permissions to roles
         $landlordRole->givePermissionTo($viewSitesPermission, $manageSitesPermission, $viewInvoicesPermission, $viewLeasesPermission);
         $tenantRole->givePermissionTo($viewInvoicesPermission, $viewLeasesPermission);
+        $tenantRole->givePermissionTo($viewSitesPermission);
 
         // Output the roles to verify the permissions were assigned
         $this->command->info("Permissions assigned to landlord role: " . json_encode($landlordRole->permissions->pluck('name')));
